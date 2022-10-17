9129900bd7062d8c88b61f7362463c8d83b03a10
vahnz13 <69780695+vahnz13@users.noreply.github.com>
2022-10-17 10:36:48 +0700

    change
    change

--- server/core.js
+++ server/core.js
@@ -135,7 +135,7 @@ const remakeUrlElement = async (dom,$,option)=>{
             const mapHref = parseUrl(hrefAttr);
             const protoHref = mapHref.protocol.replace(":","-");
             const pathHref = mapHref.pathname+mapHref.query;
-            const dataReplace = option.origin+"/host-"+protoHref+hostnameHref+pathHref;
+            const dataReplace = option.origin+"/solve-"+protoHref+hostnameHref+pathHref;
             $(el).attr(option.remake,dataReplace);
           };
         };
@@ -207,7 +207,7 @@ app.use( async (req,res,next)=>{
     if("/ping"==req.url){
       res.status(200);
       res.send("ok");
-    }else if(req.url.indexOf(dataSetting["name-folder-sitemap"])>0&&req.url.indexOf(".xml")>0&&req.url.indexOf("/host-")==-1&&req.method === "GET"){
+    }else if(req.url.indexOf(dataSetting["name-folder-sitemap"])>0&&req.url.indexOf(".xml")>0&&req.url.indexOf("/solve-")==-1&&req.method === "GET"){
       let statusSitemap=false;
       let linkSubSitemap="";
       targetSitemap.forEach(function(a){
@@ -259,7 +259,7 @@ app.use( async (req,res,next)=>{
         "content-type": "text/plain; charset=UTF-8"
       });
       res.end(data);
-    }else if (req.url.split("/assets/")[1] == undefined == false && req.method === "GET" && req.url.indexOf("/host-")==-1) {
+    }else if (req.url.split("/assets/")[1] == undefined == false && req.method === "GET" && req.url.indexOf("/solve-")==-1) {
       let dataFile = req.url.split("/assets/")[1];
       if(dataFile.length>0){
         let files = await getListFile("assets");
@@ -293,11 +293,11 @@ app.use( async (req,res,next)=>{
         let linkPost = await dataSetting["target"]+"/"+urlPost;
         let statusOrigin=true;
         let dataOrigin="";
-        if(urlPost.indexOf("host-")==0){
-          let dataLink=await urlPost.split("host-")[1];
+        if(urlPost.indexOf("solve-")==0){
+          let dataLink=await urlPost.split("solve-")[1];
           linkPost=await dataLink.replace("https-","https://").replace("http-","http://");
           statusOrigin=false;
-          dataOrigin=await "host-"+parseUrl(linkPost).origin.replace("https://","https-").replace("http://","http-");
+          dataOrigin=await "solve-"+parseUrl(linkPost).origin.replace("https://","https-").replace("http://","http-");
         };
         if(isUrl(linkPost)){
           let getInfo = await curlLink(linkPost);
